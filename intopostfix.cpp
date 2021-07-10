c40f9402b7b7bd2c378ff2fce661486e4e44673b
dwikurniawan2074 <83540724+dwikurniawan2074@users.noreply.github.com>
2021-07-10 22:11:09 +0700

    Update intopostfix.cpp

--- intopostfix.cpp
+++ intopostfix.cpp
@@ -1,1 +1,40 @@
+// Nama anggota kelompok :
+// Nur Setiowati 2017051013
+// Dwi Kurniawan 2017051074
+// Satria Sapta Nugraha 2017051022
 
+#include <bits/stdc++.h>
+using namespace std;
+
+bool oibreoir(char r){
+    if(r == '+' || 
+       r == '-' || 
+       r == '*' || 
+       r == '/' || 
+       r == '%'   ){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int prioritet(string r){
+    if(r == "+" ||
+       r == "-"   ){
+        return 1;
+    }
+    else if(r == "*" ||
+            r == "/" ||
+            r == "%"   ){
+        return 2;
+    }
+    return 0;
+}
+
+bool grande(string r, string s){
+    int Nilai1, Nilai2;
+    Nilai1 = prioritet(r); Nilai2 = prioritet(s);
+    
+    return Nilai1 <= Nilai2;
+}
