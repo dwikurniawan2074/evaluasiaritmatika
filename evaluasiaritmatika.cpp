48bf4d65a88ebbd458e5a74bfcc68113740fcfca
dwikurniawan2074 <83540724+dwikurniawan2074@users.noreply.github.com>
2021-07-10 19:00:51 +0700

    Create evaluasiaritmatika.cpp
    menambahkan nama anggota kelompok dan protoype fungsi

---
+++ evaluasiaritmatika.cpp
@@ -0,0 +1,19 @@
+// Nama Anggota Kelompok :
+// Nur Setiowati - 2017051013
+// Dwi Kurniawan - 2017051013
+// Satria Sapta Nugraha - 2017051022
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void inToPost(string str, int len);
+int isOpr(char ch);
+bool isNum(char ch);
+void postEval(string str, int len);
+int hitung(int a, int b, char opr);
+
+int main(){
+  
+  //enter code here
+}
