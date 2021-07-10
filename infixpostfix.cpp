6d3f4c895d8761008d12872b2900e9e06ac59ab4
dwikurniawan2074 <83540724+dwikurniawan2074@users.noreply.github.com>
2021-07-10 22:37:07 +0700

    Update infixpostfix.cpp

--- infixpostfix.cpp
+++ infixpostfix.cpp
@@ -1,1 +1,33 @@
+// Nama anggota kelompok :
+// Nur Setiowati 2017051013
+// Dwi Kurniawan 2017051074
+// Satria Sapta Nugraha 2017051022
 
+#include <bits/stdc++.h>
+using namespace std;
+
+bool oibreoir(char r){
+    if(r == '+' || 
+       r == '-' || 
+       r == '*' || 
+       r == '/' || 
+       r == '%'   ){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int prioritet(string r){
+    if(r == "+" ||
+       r == "-"   ){
+        return 1;
+    }
+    else if(r == "*" ||
+            r == "/" ||
+            r == "%"   ){
+        return 2;
+    }
+    return 0;
+}
