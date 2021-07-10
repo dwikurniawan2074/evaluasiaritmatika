1c181125c6100a03e9206f629ecf5b175c02d208
dwikurniawan2074 <83540724+dwikurniawan2074@users.noreply.github.com>
2021-07-10 23:07:55 +0700

    Update evaritmatika.cpp
    penambahan pengkondisian

--- evaritmatika.cpp
+++ evaritmatika.cpp
@@ -1,1 +1,28 @@
+// Nama anggota kelompok :
+// Nur Setiowati 2017051013
+// Dwi Kurniawan 2017051074
+// Satria Sapta Nugraha 2017051022
 
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isOperator(char x){
+    if(x == '+' || x == '-' ||      
+       x == '*' || x == '/' ||
+       x == '%'){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+int putFirst(string x){
+    if(x == "+" || x == "-"){
+        return 1;
+    }
+    if(x == "*" || x == "/" || 
+       x == "%"){
+        return 2;
+    }
+    return 0;
+}
