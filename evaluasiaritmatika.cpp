7bdb9266e3fc078e6c8ec6b2ef8f90514f50e088
nursetiowati2013 <83541218+nursetiowati2013@users.noreply.github.com>
2021-07-10 19:05:19 +0700

    Update evaluasiaritmatika.cpp
    penambahan fungsi untuk mengubah infix menjadi postfix

--- evaluasiaritmatika.cpp
+++ evaluasiaritmatika.cpp
@@ -24,6 +24,74 @@ int main(){
   //enter code here
 }
 
+void inToPost(string str, int len) {
+	struct stack stk;
+	stk.index = 0; // intial index stack
+	stk.ch[stk.index] = '#';
+	stk.index++;
+	string postfix = "";
+	int jml=0, num, postLen;
+	
+	for(int i=0; i<len; i++) {
+		if(isNum(str[i])) {
+			num = str[i] - cnum;
+			if(isNum(str[i+1])) {
+				jml = (jml * 10) + (num * 10);
+			}
+			else {
+				jml += num;
+				edt[index].ch = dt_ch[index];
+				edt[index].num = jml;
+				postfix += edt[index].ch;
+//				cout << edt[index].ch <<  edt[index].num << endl;
+				index++;
+				jml = 0;
+			}
+		}
+		else {
+			if(str[i]=='(') {
+				stk.ch[stk.index] = str[i];
+				stk.index++;
+			}
+			elif(str[i]==')') {
+				while(stk.ch[stk.index-1]!='#' && stk.ch[stk.index-1]!='(') {
+					postfix += stk.ch[stk.index-1];
+					stk.index--;
+				}
+				stk.index--;
+			}
+			else {
+				if(str[i]!=' ') {
+					if(!(isNum(str[i-1])) && str[i]=='-' && isNum(str[i+1])) {
+						edt[index].ch = dt_ch[index];
+						edt[index].num = jml;
+						postfix += edt[index].ch;
+						index++;
+					}
+					else {
+						while(stk.ch[stk.index-1]!='#' && isOpr(str[i]) <= isOpr(stk.ch[stk.index-1])) {
+							postfix += stk.ch[stk.index-1];
+							stk.index--;
+						}
+						stk.ch[stk.index] = str[i];
+						stk.index++;
+					}
+				}
+			}
+		}
+	}
+	
+	while(stk.ch[stk.index-1]!='#') {
+		postfix += stk.ch[stk.index-1];
+		stk.index--;
+	}
+	
+	cout << postfix;
+	postLen = postfix.size();
+	postEval(postfix, postLen);
+	delete &stk;
+}
+
 int isOpr(char ch) {
 	if(ch=='+' || ch=='-')
 		return 1;
