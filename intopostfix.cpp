e9c103e00d408d12c156abf406c995ac9bb17eff
nursetiowati2013 <83541218+nursetiowati2013@users.noreply.github.com>
2021-07-10 22:11:59 +0700

    Update intopostfix.cpp
    penambahan fungsi void

--- intopostfix.cpp
+++ intopostfix.cpp
@@ -38,3 +38,62 @@ bool grande(string r, string s){
     
     return Nilai1 <= Nilai2;
 }
+
+vector <string> infix; vector <string> postfix; string temp; vector <string>::iterator remake; vector <string>::iterator itu;
+
+void MasukkanData(){
+    string khayt;
+    char harf;
+    
+    while(cin.get(harf)){
+        if(harf == '\n'){
+            break;
+        }
+        if(harf != ' '){
+            khayt.push_back(harf);
+        }
+    }
+    int length = khayt.length();
+
+    for(int i = 0; i < length; i++){
+        if(khayt[i] == '(' ||
+           khayt[i] == ')'){
+            if(temp.length() != 0){
+                infix.push_back(temp);
+                temp.clear();
+            }
+            infix.push_back(khayt.substr(i, 1));
+        }
+        if(isdigit(khayt[i]) ||
+           (khayt[i] == '-' &&
+            i == 0 &&
+            isdigit(khayt[i+1]))){
+            
+            temp.push_back(khayt[i]);
+            continue;
+        }
+        if(oibreoir(khayt[i])){
+            if(temp.length() != 0){
+                
+                infix.push_back(temp);
+                temp.clear();
+            }
+            if((khayt[i] == '-' &&
+                oibreoir(khayt[i-1])) ||
+               (khayt[i] == '-' &&
+                i == 0 && khayt[i+1] == '(') ||
+               (khayt[i] == '-' &&
+                khayt[i-1] == '(')){
+                
+                infix.push_back("-1");
+                infix.push_back("*");
+            }else{
+                infix.push_back(khayt.substr(i, 1));
+            }
+        }
+    }
+    if(temp.length() != 0){
+        infix.push_back(temp);
+        temp.clear();
+    }
+}
